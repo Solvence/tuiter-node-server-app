d2831271bf973a38029ab3628a543099a7746752
Anthony Mu <anthonyhmu@gmail.com>
2022-12-19 11:55:26 -0600

    a8 complete

---
+++ controllers/tuits/tuits.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        "_id": 123, "topic": "Space", "userName": "SpaceX",
+        "handle": "@spacex", "time": "2h", "image": "../../images/relativity-space-logo.jpg",
+        "title": "SpaceX's Mission",
+        "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
+        "liked": true, "likes": 2,
+        "replies": 2, "retuits": 3, "dislikes": 0
+    },
+    {
+        "_id": 234, "topic": "Traffic", "userName": "The Boring Company",
+        "handle": "@boringcompany", "time": "4h", "image": "../../images/tesla-logo.png",
+        "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
+        "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
+        "liked": false, "likes": 4,
+        "replies": 5, "retuits": 6, "dislikes": 0
+    },
+];
